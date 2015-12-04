32cf5356a9a8a9dbe785dfb8d8ca46694540ef6f
jack <1432390933@qq.com>
2015-12-04 18:06:31 +0800

    2

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -4,8 +4,7 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
 //        解决跨域请求问题
-        header('Access-Control-Allow-Origin:http://angular_1.0_client.com');
-        header("Content-Type: application/json; charset=UTF-8");
+        $this->init();
         $re=M("user")->select();
         foreach($re as $key=>$value ){
             $sex=($value['f_sex']==1)?"男":"女";
@@ -15,4 +14,18 @@ class IndexController extends Controller {
         $result["list"]=$re;
         echo json_encode($result,true);
     }
+
+    //数据新增或者修改
+
+    public function moddata(){
+        $this->init();
+        echo I("post");
+    }
+
+
+    public  function  init(){
+        header('Access-Control-Allow-Origin:http://angular_1.0_client.com');
+        header("Content-Type: application/json; charset=UTF-8");
+    }
+
 }
