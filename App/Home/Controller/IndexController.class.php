610840bdc839f6caa5abf899563b40d03c048f7d
jack <1432390933@qq.com>
2015-12-21 19:06:51 +0800

    34

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -5,44 +5,67 @@ class IndexController extends Controller {
     public function index(){
 //        解决跨域请求问题
         $this->init();
+        $re=$this->getuser();
+        $result["list"]=$re;
+        $this->pushconfig($result);
+    }
+
+    public function  modData(){
+        //解决跨域请求问题
+        $this->init();
+        //获取数据添加到数s据库
+        if(I("get.f_id")==""){
+            //添加
+            $re2=M("user")->add($_GET);
+        }else{
+            //编辑
+            $re2=M("user")->save($_GET);
+        }
+
+        if($re2>0){
+            $re=$this->getuser();
+            $result["error_code"]="000000";
+            $result["list"]=$re;
+        }else{
+            $result["error_code"]="100000";
+        }
+         $this->pushconfig($result);
+    }
+
+    public function  deldata(){
+        $this->init();
+        $re=M("user")->where("f_id=".$_GET["id"])->delete();
+        if($re!=0){
+            $re=$this->getuser();
+            $result["list"]=$re;
+            $result["error_code"]="000000";
+
+        }else{
+            $result["error_code"]="100000";
+        }
+         $this->pushconfig($result);
+    }
+
+    public  function  init(){
+        header('Access-Control-Allow-Origin:*');
+        header('Access-Control-Allow-Headers: X-Requested-With');
+        header('Access-Control-Allow-Methods:GET, POST');
+    }
+
+
+    //获取表信息
+    public function  getuser(){
         $re=M("user")->select();
         foreach($re as $key=>$value ){
             $sex=($value['f_sex']==1)?"男":"女";
             $value['f_sex']=$sex;
             $re[$key]=$value;
         }
-        $result["list"]=$re;
-
-
-        echo json_encode($result,true);
+        return $re;
     }
 
-    public function  modData(){
-
-        //解决跨域请求问题
-        $this->init();
-        echo var_dump(I("post.msg"));
-        exit;
-        //获取数据添加到数据库
-        $map['f_id']=I("post.f_id");
-        $map['f_name']=I("post.f_name");
-        $map['f_sex']=I("post.f_sex");
-        $map['f_phone']=I("post.f_phone");
-
-        if(I("post.f_id")){
-            //添加
-            $re2=M("user")->add($map);
-        }else{
-            //编辑
-            $re2=M("user")->save($map);
-
-        }
-    }
-
-    public  function  init(){
-        header('Access-Control-Allow-Origin:http://angular_1.0_client.com');
-        header('Access-Control-Request-Method: GET, POST');
-        header('Access-Control-Allow-Credentials: true');
+    public function  pushconfig($data){
+        echo json_encode($data,true);
     }
 
 }
